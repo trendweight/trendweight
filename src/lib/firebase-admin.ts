1c6d264a890c8c40a5351b605af4e44e786beea8
Erv Walter <erv@ewal.net>
2021-01-01 22:25:23 +0000

    debugging private key

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -1,12 +1,16 @@
 import admin from "firebase-admin";
 
+const config = {
+  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+  privateKey: process.env.FIREBASE_PRIVATE_KEY,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+};
+
+console.log(config.privateKey);
+
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.cert({
-      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY,
-      projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-    }),
+    credential: admin.credential.cert(config),
   });
 }
 
